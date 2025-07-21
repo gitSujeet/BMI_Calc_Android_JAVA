51d89ce8a8e9ab18f229c5fafcf2a57380cb456e
Sujeet Kumar <sujeetkumarofficial1@gmail.com>
2025-07-21 22:28:28 +0530

    Updated logic.

--- app/src/main/java/com/sujeetk/bmicalculatorjava/MainActivity.java
+++ app/src/main/java/com/sujeetk/bmicalculatorjava/MainActivity.java
@@ -1,7 +1,6 @@
 package com.sujeetk.bmicalculatorjava;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
